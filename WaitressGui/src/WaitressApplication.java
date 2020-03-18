8164c9659fb613efbf6d3cf0c0cee3ab5e84e02f
michiellegters2 <michiellegters2@gmail.com>
2020-03-18 12:06:57 +0100

    Waitress screen

---
+++ WaitressGui/src/WaitressApplication.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class WaitressApplication extends Application
+{
+
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws IOException
+	{
+		Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+
+		Scene scene = new Scene(root);
+
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Ober");
+		primaryStage.sizeToScene();
+		primaryStage.show();
+	}
+}
